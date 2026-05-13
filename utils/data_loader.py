27a0229323e22ce9e3deaf91d7fde804463f79be
nirv3d <nirved@nirved.com>
2026-05-13 22:17:10 +0530

    testing

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -2,5 +2,5 @@ import numpy as np
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
 
-print("TensorFlow loaded successfully!") HELLO
+print("TensorFlow loaded successfully!") 
 
