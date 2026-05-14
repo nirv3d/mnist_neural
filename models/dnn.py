b0cd17862cc2f1d4c87302935df2e5dddba8674c
nirv3d <nirved@nirved.com>
2026-05-14 19:29:43 +0530

    dnn model done

--- models/dnn.py
+++ models/dnn.py
@@ -12,3 +12,28 @@ def build_model() -> Sequential:
     model.add(Dense(256, activation="relu"))
     model.add(BatchNormalization())
     model.add(Dropout(0.3))
+
+    model.add(Dense(128, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+
+    model.add(Dense(64, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.3))
+
+    model.add(Dense(10, activation="softmax"))
+
+
+    model.compile(
+        optimizer=Adam(learning_rate=0.001),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
+
+
+CLASS_NAMES = [
+    "T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+    "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"
+]
