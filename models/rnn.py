88aa6f4180dee7edce0a01158b318f21e0877601
nirv3d <nirved@nirved.com>
2026-05-22 22:39:19 +0530

    working on RNN

--- models/rnn.py
+++ models/rnn.py
@@ -6,7 +6,11 @@ def build_model() -> Sequential:
     model = Sequential("RNN")
 
     model.add(SimpleRNN(128, activation="tanh", return_sequences=True, input_shape=(28, 28)))
-    
+
+    model.add(SimpleRNN(64, activation="tanh", return_sequences=False))
+
+    model.add(Dropout(0.3))
+
 
 
 
